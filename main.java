a68eb4a47216f509bf1aab596b1c7b6ad8c3ac9d
oli15238 <oli15238@uvg.edu.gt>
2016-07-27 09:39:47 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 public class Principal{
     public Principal(){}
     public static void main(String[] args){
-    
+        //Metodo Generate Random
+        //Metodo Read del archivo
+        //Pequeño menu para ver por que metodo de ordenamiento se ordenará el archivo
     }
 }
