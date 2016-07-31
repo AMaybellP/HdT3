db5717561ccae2bb35673d8c2720f23891e968ad
Gladys20 <dela15755@uvg.edu.gt>
2016-07-31 08:22:03 -0600

    Update Ordenamiento.java

--- Ordenamiento.java
+++ Ordenamiento.java
@@ -44,4 +44,38 @@ public class Ordenamiento {
 		mergeSortRecursive(data,new int[n],0,n-1);
 	}
 
+
+public static void selectionSort(int data[], int n){
+
+	int numUnsorted = n;
+	int index; 
+	int max; 
+
+	while (numUnsorted > 0){
+
+	max = 0;
+	for (index = 1; index < numUnsorted; index++){
+
+	if (data[max] < data[index]) max = index;
+	}
+
+	swap(data,max,numUnsorted-1);
+	numUnsorted--;
+	}
+}
+
+void selectionSort(int data, int n) {
+      int i, j, index, max;    
+      for (i = 0; i < n - 1; i++) {
+            index = i;
+            for (j = i + 1; j < n; j++)
+                  if (data[j] < data[index])
+                        index = j;
+            if (index != i) {
+                  max = data[i];
+                  data[i] = data[index];
+                  data[index] = max;
+            }
+      }
+}
 }
